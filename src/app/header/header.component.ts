ef7a3db309ee1e17ac16a17c6ff6efaa116e9d6e
Giovanni Rosa <g.rosa@cinq.com.br>
2019-10-16 14:53:13 -0300

    more

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -7,8 +7,13 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() changePage = new EventEmitter<string>();
 
   constructor() {}
 
   ngOnInit() {}
+
+  onPageChanged(page) {
+    this.changePage.emit(page);
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredient = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addIngredient(name: string, amount: number) {
+    this.newIngredient.emit(new Ingredient(name, amount));
   }
-
 }
