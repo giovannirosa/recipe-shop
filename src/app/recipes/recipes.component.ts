abbe26d309e761f7f58dd217ca59b3023160e1db
Giovanni Rosa <g.rosa@cinq.com.br>
2019-10-22 19:38:44 -0300

    Observables

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,26 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { Recipe } from "./recipe.model";
-import { RecipeService } from "./recipe.service";
-import { Router } from "@angular/router";
 
 @Component({
   selector: "app-recipes",
   templateUrl: "./recipes.component.html",
-  styleUrls: ["./recipes.component.css"],
-  providers: [RecipeService]
+  styleUrls: ["./recipes.component.css"]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe = null;
+  constructor() {}
 
-  constructor(private recipeService: RecipeService, private router: Router) {}
-
-  ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => (this.selectedRecipe = recipe)
-    );
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.selectedRecipe = recipe;
-  }
+  ngOnInit() {}
 }
